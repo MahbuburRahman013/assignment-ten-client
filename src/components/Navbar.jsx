57dd1aff7f07ba92f00dc74ca0f70ea0efeb9ff5
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-10-18 21:42:53 +0600

    added light and dark theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom'
 import './navbar';
+import { FiSun } from 'react-icons/fi';
+import { BsMoon } from 'react-icons/bs';
 
 const Navbar = () => {
+const [isTrue,setIsTrue] = useState(true)
+
+const handleLight = () =>{
+    document.querySelector('html').setAttribute('data-theme',"dark")
+    setIsTrue(false)
+}
+const handleDark = () =>{
+    document.querySelector('html').setAttribute('data-theme',"light")
+    setIsTrue(true)
+}
+
     return (
-        <div className="container mx-auto">
+        <div className='border-b-2 border-blue-400'>
+            <div className="container mx-auto">
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -22,6 +37,13 @@ const Navbar = () => {
                         <img className="w-10" src="https://i.ibb.co/3fP9mBm/sm-5ad1c8addfe79-removebg-preview-1-removebg-preview.png" alt="" />
                         <h1 className="font-semibold text-3xl uppercase">Road<span className="font-bold text-blue-700">Wiz</span></h1>
                     </div>
+                    <div className='ml-10 border-2 border-blue-500 rounded-full px-2 pt-1 pb-[2px]'>
+                        {
+                            isTrue?
+                            <button onClick={handleLight}><BsMoon></BsMoon></button>:
+                            <button onClick={handleDark}><FiSun></FiSun></button>
+                        }
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul id='navbar' className="menu menu-horizontal gap-x-5 px-1">
@@ -37,6 +59,7 @@ const Navbar = () => {
                 </div>
             </div>
 
+        </div>
         </div>
     );
 };
